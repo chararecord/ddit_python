2a5d52b740b7769dd172bd8c4aa120f41c657daf
chararecord <any_think@naver.com>
2022-12-15 21:10:06 +0900

    221215 목

--- HELLO_PYTHON/day17/myflask.py
+++ HELLO_PYTHON/day17/myflask.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
-from flask.globals import request
-from flask.json import jsonify
+from flask import Flask, jsonify, render_template
+from subprocess import call
+from flask_socketio import SocketIO, send
+
 app = Flask(__name__)
+app.secret_key = "mysecret"
+
+socket_io = SocketIO(app)
+
+@app.route('/chat2')
+def chat2():
+    return render_template('chat2.html')
 
 @app.route('/')
-@app.route('/main')
-def teacher():
-    return render_template('main.html')
+@app.route('/allready')
+def allready():
+    return render_template('allready.html')
+
+@app.route('/canvas')
+def canvas():
+    return render_template('canvas.html')
+
+@socket_io.on("message")
+def request(message):
+    print("message : "+ message)
+    mydict = dict()
+    mydict['message'] = message
+    send(mydict, broadcast=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socket_io.run(app,debug=True,host="0.0.0.0",port=8070)
